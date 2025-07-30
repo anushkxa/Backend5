cd1279a0101f5e6189915b4c0515940ee346c20e
anushkxa <anushka082006@gmail.com>
2025-07-30 23:39:33 +0530

    Initialising

---
+++ index.js
@@ -0,0 +1,52 @@
+const{ faker } =require("@faker-js/faker");
+const mysql=require('mysql2');
+const express=require("express");
+const app=express();
+app.port=8080;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: '',
+});
+let getRandomUser=()=>{
+    return[
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.number.int(),
+    ];
+};
+ 
+
+// try {
+//   connection.query(q, [data] ,(err,res)=>{
+//   if(err) throw err;
+//   console.table(res);
+
+// });
+
+// } catch(err) {
+//   console.log(err);
+// }
+// connection.end();
+
+app.get("/",(req,res)=>{
+  let q="SELECT count(*) from user";
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      let count=result[0]["count(*)"];
+      res.render("home.ejs",{count});
+    })
+  }catch(err){
+    console.log(err);
+    res.send("some error is DB");
+  }
+})
+
+app.listen("8080",()=>{
+  console.log("Port is Listening");
+});
+
---
+++ schema.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (id,username, email, age) VALUES
+('1asjfkrfr','Anushka', 'anushka@example.com', 25);
