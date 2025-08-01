1b2ae9fba39ab182d56018662653f2e4092c0401
anushkxa <anushka082006@gmail.com>
2025-08-01 20:12:21 +0530

    Update Route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express=require("express");
 const app=express();
 app.port=8080;
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
+
 const path=require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
@@ -22,7 +26,6 @@ let getRandomUser=()=>{
         faker.number.int(),
     ];
 };
- 
 
 // try {
 //   connection.query(q, [data] ,(err,res)=>{
@@ -63,7 +66,71 @@ app.get("/user",(req,res)=>{
   }
 })
 
+app.get("/user/:id/edit",(req,res)=>{
+  let {id}=req.params;
+  let q=`SELECT * from USER WHERE id='${id}'`;
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      let user=result[0];
+      res.render("edit.ejs",{user});
+    })
+  }catch(err){
+    console.log(err);
+    res.send("some error is DB");
+  }
+})
+
+
+// app.patch(("/user/:id"),(req,res)=>{
+//   let {id}=req.params;
+//   let {email:Formemail,username:newUsername}=req.body;
+//   let q=`SELECT * from USER WHERE id='${id}'`;
+//   try{
+//     connection.query(q,(err,result)=>{
+//       if(err) throw err;
+//       let user=result[0];
+//       if(Formemail != user.email){
+//         res.send("Incorrect MAIL");
+//       }else{
+//         let q2=`UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+//         connection.query(q2,(err, result)=>{
+//           if(err) throw err;
+//         });
+//         res.redirect("/user");
+//       }
+//     })
+//   }
+//   catch(err){
+//     console.log(err);
+//     res.send("some error is DB");
+//   }
+// });
+
+app.patch("/user/:id",(req,res)=>{
+  let {id} =req.params;
+  let{email:formemail, username:newUsername}=req.body;
+  let q=`SELECT * FROM user WHERE id='${id}'`;
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      let user = result[0];
+      if(formemail != user.email){
+        res.send("Wrong Password");
+      }else{
+        let q2=`UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+        connection.query(q2,(err,result)=>{
+          if(err) throw err;
+          res.redirect("/user");
+        });
+      }
+    })
+  }catch(err){
+    console.log(err);
+    res.send("error is DB");
+  }
+});
+
 app.listen("8080",()=>{
   console.log("Port is Listening");
 });
-
